d2923ccd406817d56e1831a66c28b45e07fd02f6
Vincent Mills <vincentmills273@gmail.com>
2022-02-11 01:49:42 -0500

    Finished encoding for polybius

--- src/polybius.js
+++ src/polybius.js
@@ -8,30 +8,65 @@ const polybiusModule = (function () {
 
   const base = 'a'.charCodeAt()
   const letter_count = 26
+  const pivot = 'j'
+  const pivot_to = 'i'
 
   const squareTable = 
   [
-    [ 'a', 'b', 'c', 'd', 'e' ]
-    [ 'f', 'g', 'h', 'i', 'k' ]
-    [ 'l', 'm', 'n', 'o', 'p' ]
-    [ 'q', 'r', 's', 't', 'u' ]
+    [ 'a', 'b', 'c', 'd', 'e' ],
+    [ 'f', 'g', 'h', 'i', 'k' ],
+    [ 'l', 'm', 'n', 'o', 'p' ],
+    [ 'q', 'r', 's', 't', 'u' ],
     [ 'v', 'w', 'x', 'y', 'z' ]
   ]
 
-  const toLetter = (num) => {
-    const row = Math.floor(num / 10) - 1 , col = num % 10 - 1
-    if (row > 4 || col > 4)
-      throw
-    return squareTable[row][col]
+  const isLetter = function(char)
+  {
+    const letterIndex = char.charCodeAt() - base
+    return letterIndex >= 0 && letterIndex < letter_count
   }
 
-  const letterToNumber = (char) => {
-    return char.charCodeAt() - base
-  }
+  const toNumber = function(char)
+  {
+    const findColumn = function(row)
+    {
+      return squareTable[row].findIndex((val) => val == char)
+    }
 
+    const findRow = function(row)
+    {
+      const column = findColumn(row)
+      if(column != -1)
+      {
+        return `${column + 1}${row + 1}`
+      }
+      return undefined
+    }
+
+    if (!isLetter(char)) return char
+
+    if (char == pivot) char = pivot_to
+
+    for (let row = 0; row < squareTable.length; ++row)
+    {
+      const res = findRow(row)
+      if(res)
+        return res
+    }
+  }
 
   function polybius(input, encode = true) {
     // your solution code here
+    input = input.toLowerCase()
+    str = ""
+
+    for (let index in input)
+    {
+      if (encode)
+        str = `${str}${toNumber(input[index])}`
+    }
+
+    return str
   }
 
   return {
--- test/polybius.test.js
+++ test/polybius.test.js
@@ -10,9 +10,23 @@ describe ("polybius() testing", () => {
             const expected = "4432423352125413"
             expect(actual).to.equal(expected)
         })
+
+        it("should ignore capitalization", () => 
+        {
+            const actual = polybius("Thinkful")
+            const expected = "4432423352125413"
+            expect(actual).to.equal(expected)
+        })
+
         it("should leave in spaces", () => {
-            const actual = polybius("Hello World")
+            const actual = polybius("hello World")
             const expected = "3251131343 2543241341"
+            expect(actual).to.equal(expected)
+        })
+
+        it("should encode both i and j as the same number", () => {
+            const actual = polybius("the quick brown fox jumped over the lazy dog")
+            const expected = "443251 1454423152 2124432533 124335 425423535141 43155124 443251 13115545 414322"
             expect(actual).to.equal(expected)
         })
     })
