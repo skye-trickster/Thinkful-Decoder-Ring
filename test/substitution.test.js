48d7633e46683317c33a394dabdaca868d05113a
Vincent Mills <vincentmills273@gmail.com>
2022-02-11 04:01:18 -0500

    Created substitution tests for exceptions

--- test/substitution.test.js
+++ test/substitution.test.js
@@ -1,1 +1,30 @@
 // Write your tests here!
+// Write your tests here!
+const { expect } = require("chai")
+const { substitution } = require("../src/substitution.js")
+
+describe ("substitution() function handling", () => {
+    describe ("substitution() exception handling:", () => {
+        it("should return false if there is no alphabet code", () => {
+            const actual = substitution('thinkful')
+            expect(actual).to.be.false
+        })
+        it("should return false if the alphabet code is too short", () => {
+            const actual = substitution("thinkful", "short"); //> false
+            expect(actual).to.be.false
+        })
+    
+        it("should return false if the alphabet code is too long", () => {
+            const actual = substitution("thikful", "qwertyuiopasdfghjklzxcvbnm,./")
+            expect(actual).to.be.false
+        })
+    
+        it("should return false if the alphabet code has repeated characters", () => {
+            const actual = substitution("thikful", "abcabcabcabcabcabcabcabcyz")
+            expect(actual).to.be.false
+    
+        })
+    
+    })
+})
+
